6f7bb1a13339abd67f059598d861c5509af90e7c
Akshay Patil <akshaympatil@coep.sveri.ac.in>
2023-07-31 10:53:05 +0530

    Add files via upload

---
+++ frontend/src/APIKeys/placesAPIKey.js
@@ -0,0 +1,20 @@
+const placesAPIKey = "AIzaSyBcpoIXwX7U8EdHMQBpCJERxmIUNHfVRCE"
+
+export default placesAPIKey
+
+
+// import React from 'react';
+// import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-google-places-autocomplete';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <PlacesAutocomplete
+//         apiKey="AIzaSyBcpoIXwX7U8EdHMQBpCJERxmIUNHfVRCE"
+//         // other props
+//       />
+//     </div>
+//   );
+// };
+
+// export default App;
---
+++ frontend/src/APIKeys/weatherAPIKey.js
@@ -0,0 +1,3 @@
+const weatherAPIKey = "7a02d673ae1351d1b88ad8ffefc16d24"
+
+export default weatherAPIKey
